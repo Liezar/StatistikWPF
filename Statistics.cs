a2eb0ee943fe6d142ea345fccb849dbeeb15c4dc
Liezar <edvin.salminen@gmail.com>
2022-11-07 15:54:43 +0100

    Gjort knapparnas hörn runda, samt lagt till en hover effect på dem

--- Statistics.cs
+++ Statistics.cs
@@ -10,7 +10,7 @@ namespace WpfApp1
     public class Statistics
     {
         //Läser in json-filen och sparar det i en array som används i resten av programmet.
-        private static int[] _jsonNumbers
+        private static int[] JsonNumbers
         {
             get
             {
@@ -24,7 +24,7 @@ namespace WpfApp1
         {
             var keyList = new List<int>();
 
-            var counts = _jsonNumbers.GroupBy(i => i).Select(group => new { group.Key, Count = group.Count() });
+            var counts = JsonNumbers.GroupBy(i => i).Select(group => new { group.Key, Count = group.Count() });
 
             var itemsMax = counts.Where(x => x.Count == counts.Max(y => y.Count));
 
@@ -39,16 +39,16 @@ namespace WpfApp1
         //metod för beräkning av standardavikelse
         private static double GetStandardDeviation()
         {
-            double deviation = _jsonNumbers.Average();
-            double avarageDeviation = _jsonNumbers.Select(val => (val - deviation) * (val - deviation)).Sum();
-            double standardDeviation = Math.Round(Math.Sqrt(avarageDeviation / _jsonNumbers.Length), 1);
+            double deviation = JsonNumbers.Average();
+            double avarageDeviation = JsonNumbers.Select(val => (val - deviation) * (val - deviation)).Sum();
+            double standardDeviation = Math.Round(Math.Sqrt(avarageDeviation / JsonNumbers.Length), 1);
 
             return standardDeviation;
         }
         //metod för beräkning av medianvärde
         private static int GetMedian()
         {
-            var sortednums = _jsonNumbers.OrderBy(number => number).ToList();
+            var sortednums = JsonNumbers.OrderBy(number => number).ToList();
             //hittar talet i mitten på talserien:
             int itemIndex = sortednums.Count / 2;
             //om jämnt värde i mitten på talserien:
@@ -65,28 +65,28 @@ namespace WpfApp1
         private static double GetMean() // Metoden beräknar medelvärdet av datan från Json-filen.
         {
             double sum = 0;
-            foreach (int x in _jsonNumbers)
+            foreach (int x in JsonNumbers)
             {
                 sum += x;
             }
-            return Math.Round(sum / _jsonNumbers.Length, 1);
+            return Math.Round(sum / JsonNumbers.Length, 1);
         }
         public static dynamic DescriptiveStatistics() // Metoden anropar returvärdet från uträkningsmetoderna och skapar en Dictionary som returneras.
         {
             dynamic allData = new Dictionary<string, object>() { };
 
-            StringBuilder sb = new StringBuilder();
+            var sb = new StringBuilder();
             foreach (var item in Mode())
             {
                 sb.Append($"{item} ");
             }
 
-            allData.Add("Maximum", _jsonNumbers.Max());
-            allData.Add("Minimum", _jsonNumbers.Min());
+            allData.Add("Maximum", JsonNumbers.Max());
+            allData.Add("Minimum", JsonNumbers.Min());
             allData.Add("Medelvärde", GetMean());
             allData.Add("Median", GetMedian());
             allData.Add("Typvärde", sb);
-            allData.Add("Variationsbredd", _jsonNumbers.Max() - _jsonNumbers.Min());
+            allData.Add("Variationsbredd", JsonNumbers.Max() - JsonNumbers.Min());
             allData.Add("Standardavvikelse", GetStandardDeviation());
 
             return allData;
